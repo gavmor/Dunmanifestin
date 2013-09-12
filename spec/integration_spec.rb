d105731856a37e6607c599dcf2164269bd99ccd3
GavMo <quavmo@gmail.com>
2013-09-11 19:41:32 -0700

    Adds specs for simple script usage

---
+++ spec/integration_spec.rb
@@ -0,0 +1,25 @@
+require 'rspec'
+
+describe 'generate' do
+  it 'produces a string of text' do
+    text = `./generate.sh`
+    text.should_not be_empty
+  end
+
+  describe 'loading a genre' do
+    let(:scifi) {  }
+
+    around do |example|
+      `mkdir ./lists/scifi`
+      `echo "|root\nraygun" > ./lists/scifi/root.list`
+      example.run
+      `rm -r ./lists/scifi`
+      `rm ./scifi.list`
+    end
+
+    it 'overwrites default lists' do
+      text = `./generate.sh -g scifi`.chomp
+      text.should == 'raygun'
+    end
+  end
+end
