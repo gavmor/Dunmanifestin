42cef1639bdf814d8f952295b0eb65a9bf26e062
Gavin Morgan <quavmo@gmail.com>
2014-11-26 13:10:20 -0800

    Stringify root phrase classes instead of inspecting them

--- lib/dunmanifestin.rb
+++ lib/dunmanifestin.rb
@@ -1,11 +1,10 @@
-$:.unshift File.dirname(__FILE__) # For dev/use when no gem is installed
+# $:.unshift File.dirname(__FILE__) # For dev/use when no gem is installed
 
-require 'trollop'
-require 'dunmanifestin/member.rb'
-require 'dunmanifestin/list.rb'
-require 'dunmanifestin/generator.rb'
-require 'dunmanifestin/string.rb'
-require 'dunmanifestin/custom_inflections.rb'
-require 'dunmanifestin/phrase.rb'
-require 'dunmanifestin/manifestation.rb'
-require 'dunmanifestin/list_loader.rb'
+require_relative 'dunmanifestin/member'
+require_relative 'dunmanifestin/list'
+require_relative 'dunmanifestin/generator'
+require_relative 'dunmanifestin/string'
+require_relative 'dunmanifestin/custom_inflections'
+require_relative 'dunmanifestin/phrase'
+require_relative 'dunmanifestin/manifestation'
+require_relative 'dunmanifestin/list_loader'
