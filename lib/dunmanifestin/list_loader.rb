c512f98fb12f84fa4fc11c6de428e3b8c0773727
Gavin Morgan <gavin@firstclass.ci>
2016-09-25 00:29:20 -0700

    Protect against perpetually nil CLI args

--- lib/dunmanifestin/list_loader.rb
+++ lib/dunmanifestin/list_loader.rb
@@ -6,8 +6,8 @@ class ListLoader
   DEFAULT_GENRE = File.join(*%W(#{File.dirname(__FILE__)} .. .. default-genre))
   
   class << self
-    def load super_genre=""
-      [DEFAULT_GENRE, super_genre.split(":")]
+    def load super_genre=''
+      [DEFAULT_GENRE, (super_genre || '').split(":")]
         .flatten.compact.each(&method(:load_genre))
     end
 
