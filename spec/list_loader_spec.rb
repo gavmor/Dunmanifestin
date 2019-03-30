bb776f6eba4f001c27839c0add147d6b531df917
Gavin Morgan <morgan.gavin@gmail.com>
2019-03-29 17:26:59 -0700

    tests should know about the file extension

--- spec/list_loader_spec.rb
+++ spec/list_loader_spec.rb
@@ -3,7 +3,7 @@ require 'tmpdir'
 
 describe ListLoader do
   let(:palette_name) { 'root.dun' }
-  let(:default_palettes) do  
+  let(:default_palettes) do
     %w(doctrinist adverb abstraction root superlative verb)
   end
   let(:palette_dir) { Dir.mktmpdir }
@@ -15,9 +15,9 @@ frog
 chicken
 TXT
   end
-  
+
   before do
-    open(File.join(palette_dir, palette_name), 'w') { |f| f.puts palette_doc }
+    open(File.join(palette_dir, palette_name+'.pal'), 'w') { |f| f.puts palette_doc }
     allow(Palette).to receive(:expose)
   end
 
@@ -27,23 +27,23 @@ TXT
         expect(Palette).to receive(:expose)
           .with(something_including(palette))
       end
-      
+
       ListLoader.load
     end
-    
+
     it 'exposes the given genre' do
       expect(Palette).to receive(:expose)
         .with(something_including(palette_name))
-      
+
       ListLoader.load palette_dir
     end
-    
+
     context 'when given multiple genres' do
-      
+
       it 'exposes each in turn' do
         expect(Palette).to receive(:expose)
           .with(something_including(palette_name)).twice
-        
+
         ListLoader.load [palette_dir,palette_dir].join(':')
       end
     end
