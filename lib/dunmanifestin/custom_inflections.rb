38e718e860cb01231c3893e2e7ff7c4a631f44c6
Ben Christel <bchristel@pivotal.io>
2019-04-21 15:25:39 -0700

    Pluralize "leaf" correctly

--- lib/dunmanifestin/custom_inflections.rb
+++ lib/dunmanifestin/custom_inflections.rb
@@ -2,5 +2,6 @@ require 'active_support/inflector'
 
 ActiveSupport::Inflector.inflections do |inflect|
   inflect.irregular 'human', 'humans'
+  inflect.irregular 'leaf', 'leaves'
   inflect.plural /^(.+)z$/, '\0es'
 end
